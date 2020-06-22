38370f7ba72dcc02bb2cf9025e6f6e5df3b77555
FabriceT <fabrice.tsiava@artik-consulting.com>
2020-06-22 18:11:38 +0200

    feat : add template

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
  * Define a View AppView
  */
 const AppView = Backbone.View.extend({
-    /**  el - stands for element. Every view has a element associate in with HTML
-     *  content will be rendered.
+    /**  
+     * el - stands for element. Every view has a element associate in with HTML
+     * content will be rendered.
      * #container : html element whose id="container"
      */
     el: '#container',
+    /** 
+     * template which has the placeholder 'who' to be substituted later
+     */ 
+    template: _.template($('#hello-world-template').html()),
     /** 
      * It's the first function called when this view is instantiated.
      */
@@ -18,11 +23,14 @@ const AppView = Backbone.View.extend({
      * to push content. Like the Hello World in this case.
      */
     render: function () {
-        this.$el.html("Hello World");
+        /**
+         * replace placeholder <%= who %> with Fab in template
+         */
+        this.$el.html(this.template({who: 'Fab!'}));
     }
 });
 
 /**
- * Need to instantiate the module
+ * Need to instantiate the component
  */
 const appView = new AppView();
