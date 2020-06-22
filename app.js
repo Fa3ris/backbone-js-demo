e931f1ec73165d871ab9f82c9bda974838598246
FabriceT <fabrice.tsiava@artik-consulting.com>
2020-06-22 18:49:38 +0200

    feat: add app component

--- app.js
+++ app.js
@@ -86,14 +86,73 @@ app.TodoList = Backbone.Collection.extend({
 /**
  *  instance of the Collection
  */
-const todoList = new app.TodoList();
+app.todoList = new app.TodoList();
 
-todoList.create({title: 'Learn Backbone\'s Collection'}); // notice: that `completed` will be set to false by default.
+app.todoList.create({title: 'Learn Backbone\'s Collection'}); // notice: that `completed` will be set to false by default.
 
 const todo1 = new app.Todo({title: 'Learn Models', completed: true});
-todoList.add(todo1);
+app.todoList.add(todo1);
 
-console.log(todoList.pluck('title'));     // ["Learn Backbone's Collection", "Learn Models"]
-console.log(todoList.pluck('completed')); // [false, true]
+console.log(app.todoList.pluck('title'));     // ["Learn Backbone's Collection", "Learn Models"]
+console.log(app.todoList.pluck('completed')); // [false, true]
 
-console.log(JSON.stringify(todoList)); // "[{"title":"Learn Backbone's Collection","completed":false,"id":"d9763e99-2267-75f5-62c3-9d7e40742aa6"},{"title":"Learn Models","completed":true}]"
+console.log(JSON.stringify(app.todoList)); // "[{"title":"Learn Backbone's Collection","completed":false,"id":"d9763e99-2267-75f5-62c3-9d7e40742aa6"},{"title":"Learn Models","completed":true}]"
+
+
+// renders individual todo items list (li)
+app.TodoView = Backbone.View.extend({
+    /**
+     * parent tag is <li>
+     */
+    tagName: 'li',
+    template: _.template($('#item-template').html()),
+    render: function(){
+      this.$el.html(this.template(this.model.toJSON()));
+      return this; // enable chained calls
+    }
+});
+
+const view = new app.TodoView({model: todo});
+
+
+// renders the full list of todo items calling TodoView for each one.
+app.AppView = Backbone.View.extend({
+    el: '#app-todo',
+    initialize: function () {
+      this.input = this.$('#new-todo');
+      // when new elements are added to the collection render then with addOne
+      app.todoList.on('add', this.addOne, this);
+      app.todoList.on('reset', this.addAll, this);
+      app.todoList.fetch(); // Loads list from local storage
+    },
+    events: {
+      'keypress #new-todo': 'createTodoOnEnter'
+    },
+    createTodoOnEnter: function(e){
+      if ( e.which !== 13 || !this.input.val().trim() ) { // ENTER_KEY = 13
+        return;
+      }
+      app.todoList.create(this.newAttributes());
+      this.input.val(''); // clean input box
+    },
+    addOne: function(todo){
+      var view = new app.TodoView({model: todo});
+      $('#todo-list').append(view.render().el);
+    },
+    addAll: function(){
+      this.$('#todo-list').html(''); // clean the todo list
+      app.todoList.each(this.addOne, this);
+    },
+    newAttributes: function(){
+      return {
+        title: this.input.val().trim(),
+        completed: false
+      }
+    }
+  });
+  
+  //--------------
+  // Initializers
+  //--------------
+  
+  app.appView = new app.AppView();
